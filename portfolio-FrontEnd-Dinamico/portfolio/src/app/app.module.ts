fba687a5e1e788df3b51b38fbbf379eb5e782c36
ldlmarcelo <ldl_marcelo@hotmail.com>
2022-06-16 00:31:50 -0300

    PortfolioAvance
    componentes y servicio

--- portfolio-FrontEnd-Dinamico/portfolio/src/app/app.module.ts
+++ portfolio-FrontEnd-Dinamico/portfolio/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { PerfilComponent } from './components/acerca-de/perfil/perfil.component';
+import { SobreMiComponent } from './components/acerca-de/sobre-mi/sobre-mi.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    PerfilComponent,
+    SobreMiComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ portfolio-FrontEnd-Dinamico/portfolio/src/app/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  miPortfolio: any;
+
+  constructor(private infoPortfolio: PortfolioService) { }
+
+  ngOnInit(): void {
+    this.infoPortfolio.obtenerDatos().subscribe(info => {
+      this.miPortfolio=info;
+    })
+
+  }
+
+}
