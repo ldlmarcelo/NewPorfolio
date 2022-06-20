a542463087d006df5deb6a803c07d40200b26f21
ldlmarcelo <ldl_marcelo@hotmail.com>
2022-06-20 19:21:35 -0300

    desde cero

--- portfolio-FrontEnd-Dinamico/portfolio/src/app/app.module.ts
+++ portfolio-FrontEnd-Dinamico/portfolio/src/app/app.module.ts
@@ -1,28 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-import { PerfilComponent } from './components/acerca-de/perfil/perfil.component';
-import { SobreMiComponent } from './components/acerca-de/sobre-mi/sobre-mi.component';
-
+import { HeaderComponent } from './componentes/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AcercaDeComponent,
-    PerfilComponent,
-    SobreMiComponent,
-   
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
