dba6701f0202a8696d9272fcf0ef884b9381db3f
ldlmarcelo <ldl_marcelo@hotmail.com>
2022-06-21 20:23:47 -0300

    arreglo error no se muestra foto

--- portfolio-FrontEnd-Dinamico/portfolio/src/app/componentes/acerca-de/acerca-de.component.ts
+++ portfolio-FrontEnd-Dinamico/portfolio/src/app/componentes/acerca-de/acerca-de.component.ts
@@ -4,19 +4,18 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
 @Component({
   selector: 'app-acerca-de',
   templateUrl: './acerca-de.component.html',
-  styleUrls: ['./acerca-de.component.css']
+  styleUrls: ['./acerca-de.component.css'],
 })
 export class AcercaDeComponent implements OnInit {
-miPortfolio:any;
-  constructor(private datosPortfolio:PortfolioService) { }
+  miPortfolio: any;
+
+  constructor(private datosPortfolio: PortfolioService) {}
 
   ngOnInit(): void {
-    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+    this.datosPortfolio.obtenerDatos().subscribe((data) => {
       console.log(data);
-      this.miPortfolio=data;
+      this.miPortfolio = data;
 
-      });
-    }
+    });
   }
-
-
+}
