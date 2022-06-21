2b76cc2149ab49be5b53ab68ef449b3483608107
ldlmarcelo <ldl_marcelo@hotmail.com>
2022-06-20 21:17:53 -0300

    separp en componentes

--- portfolio-FrontEnd-Dinamico/portfolio/src/app/app.module.ts
+++ portfolio-FrontEnd-Dinamico/portfolio/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaLaboralComponent } from './componentes/experiencia-laboral/experiencia-laboral.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HardSkillsComponent } from './componentes/hard-skills/hard-skills.component';
+import { SoftSkillsComponent } from './componentes/soft-skills/soft-skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaLaboralComponent,
+    EducacionComponent,
+    HardSkillsComponent,
+    SoftSkillsComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
