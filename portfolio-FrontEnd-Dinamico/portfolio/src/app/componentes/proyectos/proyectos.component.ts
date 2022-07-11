693a11bbaebe8f6c0f7b122c154b2fa531470e41
ldlmarcelo <ldl_marcelo@hotmail.com>
2022-07-11 19:08:01 -0300

    avance implementación json

--- portfolio-FrontEnd-Dinamico/portfolio/src/app/componentes/proyectos/proyectos.component.ts
+++ portfolio-FrontEnd-Dinamico/portfolio/src/app/componentes/proyectos/proyectos.component.ts
@@ -1,15 +1,18 @@
+import { PortfolioService } from './../../servicios/portfolio.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
-  styleUrls: ['./proyectos.component.css']
+  styleUrls: ['./proyectos.component.css'],
 })
 export class ProyectosComponent implements OnInit {
-
-  constructor() { }
+  proyectosList: any;
+  constructor(private Miportfolio: PortfolioService) {}
 
   ngOnInit(): void {
+    this.Miportfolio.obtenerDatos().subscribe((data) => {
+      this.proyectosList = data.proyectosList;
+    });
   }
-
 }
